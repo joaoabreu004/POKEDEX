d211b570b954399fd299d5799d6f4da696d644fe
João Vitor de Abreu <jvabreusousa14@gmail.com>
2022-04-28 17:36:23 -0300

    Adicionando um arquivo base para conctar minha aplicação com a pokeAPI

---
+++ scripts.js/loadpk.js
@@ -0,0 +1,26 @@
+function loadpokemon(){
+    let url = 'https://pokeapi.co/api/v2/pokemon/898'; 
+    fetch(url)
+        .then((response) => {
+            return response.json(); 
+        })
+
+        .then((dados) => {
+            //O que vamos fazer com os dados em formato JSON
+            console.clear(); 
+            console.log(dados); 
+            document.querySelector(".nome").innerHTML = dados["name"]; 
+            //chave da api é "name"
+            let img = dados['sprites']['front_default'];
+            document.querySelector(".pic").setAttribute('src', img); 
+            document.querySelector(".id").innerHTML = dados["id"]; 
+        })
+
+        .catch((erro) => {
+            console.log("Erro " + erro); 
+        }); 
+    
+}
+
+
+document.querySelector(".carregar").onclick = loadpokemon;
