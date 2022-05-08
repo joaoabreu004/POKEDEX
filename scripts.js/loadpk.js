c10c8791481622b558a58637b6e2e359a55eca7a
João Vitor de Abreu <jvabreusousa14@gmail.com>
2022-05-08 10:03:09 -0300

    adicionando num de id e ajustando um único card

--- scripts.js/loadpk.js
+++ scripts.js/loadpk.js
@@ -1,5 +1,9 @@
-function loadpokemon(){
-    let url = 'https://pokeapi.co/api/v2/pokemon/172'; 
+function carregarPokemon(){
+    let valorUsuario = document.querySelector('.barradepesquisa');
+    let value = valorUsuario.value; 
+    console.log(value)
+    let url = 'https://pokeapi.co/api/v2/pokemon/' + value ;
+
     fetch(url)
         .then((response) => {
             return response.json(); 
@@ -9,10 +13,10 @@ function loadpokemon(){
             //O que vamos fazer com os dados em formato JSON
             console.clear(); 
             console.log(dados); 
-            document.querySelector(".nome").innerHTML = dados["name"]; 
+            document.querySelector("#nome").innerHTML = dados["name"]; 
             //chave da api é "name"
-            let img = dados['sprites']['front_default'];
-            document.querySelector(".pokedex__left__tela__pic").setAttribute('src', img); 
+            let img = dados['sprites']['other']['official-artwork']['front_default'];
+            document.querySelector("#pic").setAttribute('src', img); 
             document.querySelector(".id").innerHTML = dados["id"]; 
         })
 
@@ -23,4 +27,4 @@ function loadpokemon(){
 }
 
 
-document.querySelector(".carregar").onclick = loadpokemon;
+document.querySelector(".btnPesquisa").onclick = carregarPokemon;
